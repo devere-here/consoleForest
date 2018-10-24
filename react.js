db995be03fcce91187df0461c543e854bdc88bf7
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-10-24 14:24:20 -0400

    feat: adds console logs after the render function

--- react.js
+++ react.js
@@ -2,7 +2,14 @@ const vscode = require('vscode');
 
 
 function getStateInfo (line) {
+  const substring = line.trim();
+  const wordArray = substring.split(':');
   
+  return wordArray[0];
+}
+
+function createConsoleString (stateInfo, i, j) {
+  return `'this.state.${stateInfo} line ${i + j + 1}', this.state.${stateInfo}`;
 }
 
 const react = vscode.commands.registerCommand('extension.react', function () {
@@ -20,6 +27,7 @@ const react = vscode.commands.registerCommand('extension.react', function () {
           const documentData = window.activeTextEditor.document;
           const stateInfo = [];
           let isState = false;
+          let isRender = false;
           let numInsertedLines = 0;
 
           for (let i = 0; i < documentData.lineCount; i++){
@@ -29,14 +37,25 @@ const react = vscode.commands.registerCommand('extension.react', function () {
               } else if (isState) {
                 const currentStateInfo = getStateInfo(line);
                 stateInfo.push(currentStateInfo);
+                numInsertedLines++;
                 if (!line.includes('{') && line.includes('}')) { // Best practices would have the '}' on a separate line
                   isState = false;
+                  numInsertedLines--;
                 }
-                numInsertedLines++;
-              } else if (line.includes('render')) {
-                
+              } else if (line.includes('render()')) {
+                isRender = true;
+              } else if (isRender) {
+                const startColumn = line.search(/[^ ]/);
+                let currentStr = '';
+                for (let j = 0; j < numInsertedLines; j++) {
+                  const consoleStr = createConsoleString(stateInfo[j], i, j);
+                  currentStr += `console.log(${consoleStr});\n${" ".repeat(startColumn)}`;
+                }
+                let propsStr = `console.log('this.props line ${i + numInsertedLines}', this.props);\n${" ".repeat(startColumn)}`;
+                edit.insert(uri, new Position(i, startColumn), currentStr + propsStr);
+                isRender = false;
+                break;
               }
-              
           }
           workspace.applyEdit(edit);
       }
